2ed49e5a18845b5875a5e5f63468bfe649dd9ac4
Airbus5717 <abdussamadf350@gmail.com>
2023-06-04 00:45:10 +0300

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://airbus5717.github.io',
+    base: '/',
 	integrations: [mdx(), sitemap()],
 });
